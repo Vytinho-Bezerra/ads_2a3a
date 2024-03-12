87b811ae4965ba7f3c4225443ac9ee40931b1ac5
Exemplo <exemplo@email.com>
2024-03-12 10:31:22 -0300

    Todos os exemplos feitos em aula.

--- aula05_JS_parte2/exemplo7.js
+++ aula05_JS_parte2/exemplo7.js
@@ -7,10 +7,29 @@ de 35 a 40 = Obesidade II
 acima de 40 = Obesidade III
 */
 
-calcularIMC(55, 1.75);
+let res_imc = calcularIMC(55, 1.75);
+console.log(res_imc);
+classificarIMC(res_imc);
 
 function calcularIMC(p, a) {
     let imc;
     imc = p / a**2;
-    console.log(imc)
+    //console.log(imc);
+    return imc;
+}
+
+function classificarIMC(argIMC) {
+    if (argIMC < 18.5) {
+        console.log("Desnutrição");
+    } else if (argIMC < 25) {
+        console.log("Peso normal");
+    } else if (argIMC < 30) {
+        console.log("Sobrepeso");
+    } else if (argIMC < 35) {
+        console.log("Obesidade I");
+    } else if (argIMC < 40) {
+        console.log("Obesidade II");
+    } else {
+        console.log("Obesidade III");
+    }
 }
